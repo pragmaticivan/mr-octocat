270ac7d1b8d2caa1cfcb7b214959dc6787e3607f
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2019-05-08 21:49:22 -0700

    feat: add repository area component

--- src/containers/RepositoryArea/index.tsx
+++ src/containers/RepositoryArea/index.tsx
@@ -1,12 +1,40 @@
 import React from "react";
+import style from "./style.css";
+import Link from "next/link";
+import RepoCard from "../../components/RepoCard";
+
 interface Props {
+  pageTab: string | null;
 }
 
 export default class RepositoryArea extends React.Component<Props> {
   render() {
-    return(
-      <div>
-        <h1>Repos</h1>
+    const { pageTab } = this.props;
+    return (
+      <div className={style.container}>
+        <ul className={style.repoNavigation}>
+          <li className={pageTab ? "" : style.active}>
+            <Link href="/">
+              <a>Overview</a>
+            </Link>
+          </li>
+          <li className={pageTab === "repositories" ? style.active : ""}>
+            <Link href="/?tab=repositories">
+              <a>Repositories <span>8</span></a>
+            </Link>
+          </li>
+        </ul>
+
+        <div>
+          <h3>Popular repositories</h3>
+
+          <div className={style.repoCardList}>
+            <RepoCard />
+            <RepoCard />
+            <RepoCard />
+            <RepoCard />
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/containers/RepositoryArea/style.css.d.ts
@@ -0,0 +1,6 @@
+export const container: string;
+export const repo_navigation: string;
+export const repoNavigation: string;
+export const active: string;
+export const repo_card_list: string;
+export const repoCardList: string;
